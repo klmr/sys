8431fa3eed98a6664d5549c950eea985e68231ec
Konrad Rudolph <konrad.rudolph@gmail.com>
2016-02-08 14:39:36 +0100

    Add unit testing helpers

---
+++ _tests/helper-cmd.r
@@ -0,0 +1,30 @@
+cmd = modules::import('../cmdline')
+
+shows_help = function ()
+    function (x)
+        expectation(inherits(x, 'try-error') &&
+                    inherits(attr(x, 'condition'), 'sys$cmdline$help'),
+                    'does not show help', 'shows help')
+
+args_equal = function (...) {
+    expected = list(...)
+    function (actual) {
+        na = sort(names(actual))
+        ne = sort(names(expected))
+        expectation(identical(na, ne) &&
+                    identical(actual[na], expected[ne]),
+                    'arguments are not equal', 'arguments are equal')
+    }
+}
+
+construct_parse_call = function (call) {
+    call$args = call$cmdline
+    call$cmdline = NULL
+    call[[1]] = call('$', quote(cmd), quote(parse))
+    call
+}
+
+xc = function (cmdline, ...) {
+    call = construct_parse_call(match.call(expand.dots = TRUE))
+    try(eval.parent(call), silent = TRUE)
+}
