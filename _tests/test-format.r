e9be19b2a46879bcdd2fd17befb0bb00b7015270
Konrad Rudolph <konrad.rudolph@gmail.com>
2016-07-04 10:49:41 +0100

    Add more test for basic types

--- _tests/test-format.r
+++ _tests/test-format.r
@@ -13,6 +13,25 @@ test_that('numbers are formatted correctly', {
     expect_that(repr(-1.5), equals('-1.5'))
 })
 
+test_that('simple character strings are formatted correctly', {
+    expect_that(repr('foo'), equals('foo'))
+})
+
+test_that('Escape sequences are formatted correctly', {
+    expect_that(repr('"hello"'), equals('"hello"'))
+    expect_that(repr("'hello'"), equals("'hello'"))
+    expect_that(repr('foo\n\tbar'), equals('foo\n\tbar'))
+})
+
+test_that('Unicode characters are formatted correctly', {
+    uchr = '\U00DF\U00E8\U00E5\U2603'
+    expect_that(repr(uchr), equals(uchr))
+})
+
+test_that('logical values are formatted correctly', {
+    expect_that(repr(c(TRUE, FALSE)), equals('TRUE, FALSE'))
+})
+
 test_that('dispatch to `format` happens appropriately', {
     time_chr = '1973-11-29 21:33:09'
     time = as.POSIXct(time_chr)
