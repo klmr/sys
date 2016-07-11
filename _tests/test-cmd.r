37ba19b46de46f4a254eb9500f532c21d95768e4
Konrad Rudolph <konrad.rudolph@gmail.com>
2016-07-11 19:46:10 +0200

    Add test cases for cmd$parse calling

--- _tests/test-cmd.r
+++ _tests/test-cmd.r
@@ -18,3 +18,16 @@ test_that('positional arguments with dash prefix work', {
     expect_that(xc(c('--', '--foo'), arg('filename', '')),
                 args_equal(filename = '--foo'))
 })
+
+test_that('`parse` can be called with a variable', {
+    x = cmd$arg('test', '')
+    expect_that(xc('x', x), throws_error(NA))
+})
+
+test_that('`parse` can be called with fully qualified name', {
+    expect_that(xc('x', cmd$arg('test', '')), throws_error(NA))
+})
+
+test_that('`parse` can be called with unqualified name', {
+    expect_that(xc('x', arg('test', '')), throws_error(NA))
+})
