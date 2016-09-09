4a37c8b5968bd59794421b11499e1691dd020396
Konrad Rudolph <konrad.rudolph@gmail.com>
2016-09-09 14:33:03 +0100

    Fix default expected argument count

--- cmd.r
+++ cmd.r
@@ -388,7 +388,7 @@ arg = function (name, description, default, arity = 1, validate, transform) {
     result = list()
     arg_pos = 1
     short_opt_pos = 1
-    expected_args = 1
+    expected_args = 0
 
     while (i <= length(args)) {
         token = args[i]
